5e7ea39eb11b6c50a9b11250498455193a07e333
Artur Gęsiarz <126520293+arturgesiarz@users.noreply.github.com>
2025-10-15 12:23:36 +0200

    Add kmp-sharing demo

--- composeApp/src/commonMain/kotlin/com/swmansion/kmp_maps_presentation/App.kt
+++ composeApp/src/commonMain/kotlin/com/swmansion/kmp_maps_presentation/App.kt
@@ -1,49 +1,56 @@
 package com.swmansion.kmp_maps_presentation
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.swmansion.kmpsharing.Anchor
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import kmpmapspresentation.composeapp.generated.resources.Res
-import kmpmapspresentation.composeapp.generated.resources.compose_multiplatform
+import com.swmansion.kmpsharing.SharingOptions
+import com.swmansion.kmpsharing.rememberShare
 
 @Composable
 @Preview
 fun App() {
+    val share = rememberShare()
+
+
+
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
+        Box(modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.background)) {
+            Column(
+                modifier = Modifier.fillMaxSize().padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+            ) {
+                Button(
+                    onClick = {
+                        share(
+                            data = "https://swmansion.com/"
+                        )
+                    },
                 ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+                    Text(
+                        text = "Share a file!",
+                    )
                 }
             }
         }
+
     }
 }
